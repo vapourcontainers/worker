9ce7bff87b669718e720ce0f1967f73d96c625eb
XiNGRZ <hi@xingrz.me>
2023-08-21 01:57:12 +0800

    client: Query by current stage

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -7,15 +7,53 @@ const sockPath = join(rootPath, 'ws-guard.sock');
 const ws = new WebSocket(`ws+unix://${sockPath}`);
 
 ws.on('message', (message: string) => {
-  console.log(message.toString());
+  const line = message.toString();
+  console.log(line);
+
+  const event = parseEvent(line);
+  switch (event?.name) {
+    case 'stage': {
+      switch (event.data) {
+        case Stage.DOWNLOAD: {
+          ws.send(buildEvent('download-progress'));
+        } break;
+        case Stage.ENCODE: {
+          ws.send(buildEvent('format'));
+          ws.send(buildEvent('progress'));
+        } break;
+        case Stage.UPLOAD: {
+          ws.send(buildEvent('upload-progress'));
+        } break;
+      }
+    } break;
+  }
 });
 
 ws.once('open', () => {
+  ws.send(buildEvent('project'));
   ws.send(buildEvent('stage'));
-  ws.send(buildEvent('format'));
-  ws.send(buildEvent('progress'));
 });
+
+interface IEvent<T = unknown> {
+  name: string;
+  data: T | undefined;
+}
+
+function parseEvent(message: string): IEvent | undefined {
+  try {
+    const { name, data } = JSON.parse(message);
+    return { name, data };
+  } catch (e) {
+    return undefined;
+  }
+}
 
 function buildEvent(name: string, data?: unknown): string {
   return JSON.stringify({ name, data });
+}
+
+enum Stage {
+  DOWNLOAD = 'download',
+  ENCODE = 'encode',
+  UPLOAD = 'upload',
 }
